e00b841ff8e1c43039509e5af53efbc194761707
mwendwamark <mwendwamark310@gmail.com>
2025-07-15 13:04:26 +0300

    create the home products section, style it andmake it responsive.

---
+++ src/MockData/HomeProducts.jsx
@@ -0,0 +1,48 @@
+import img1 from "../assets/Full_Grilled_Chicken.jpg";
+import img2 from "../assets/Hamburger.jpg";
+import img3 from "../assets/Rice_and_meat.webp";
+import img4 from "../assets/Shawarma.jpg";
+const homeProducts = [
+  {
+    id: 1,
+    productName: "Full grilled Chicken",
+    image: img1,
+    av_rating: 4.9,
+    total_rating: 368,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti eum aperiam culpa ad alias aspernatur harum repudiandae atque vitae fugit? Fugit.",
+    price: 850,
+  },
+  {
+    id: 2,
+    productName: "Hamburger",
+    image: img2,
+    av_rating: 5.0,
+    total_rating: 219,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti eum aperiam culpa ad alias aspernatur harum repudiandae atque vitae fugit? Fugit.",
+    price: 350,
+  },
+  {
+    id: 3,
+    productName: "Rice and Beef",
+    image: img3,
+    av_rating: 4.9,
+    total_rating: 102,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti eum aperiam culpa ad alias aspernatur harum repudiandae atque vitae fugit? Fugit.",
+    price: 250,
+  },
+  {
+    id: 4,
+    productName: "Shawarma",
+    image: img4,
+    av_rating: 4.8,
+    total_rating: 412,
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti eum aperiam culpa ad alias aspernatur harum repudiandae atque vitae fugit? Fugit.",
+    price: 210,
+  },
+];
+
+export default homeProducts;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,38 @@
+// Home.jsx
 import React, { useEffect } from "react";
 import "./Home.css";
 import { NavLink } from "react-router-dom";
 import { annotate } from "rough-notation";
-import { ArrowDownRight } from "lucide-react";
+import { ArrowDownRight, ShoppingCart, Star } from "lucide-react";
 import Shawarma from "../../assets/Shawarma.jpg";
 import Pilau_and_Meat from "../../assets/Pilau_and_Meat.jpg";
 import Hamburger from "../../assets/Hamburger.jpg";
 import Full_Grilled_Chicken from "../../assets/Full_Grilled_Chicken.jpg";
 import Chicken_pieces from "../../assets/Chicken_Pieces.jpg";
 import Moriah_Fries from "../../assets/Moriah_Fries.jpg";
+import homeProducts from "../../MockData/HomeProducts";
 
 const Home = () => {
   useEffect(() => {
     const e = document.querySelector("#moriah_chicken_grill");
+    const f = document.querySelector("#home_section-heading");
     if (e) {
       const annotation = annotate(e, {
+        animate: true,
         type: "circle",
         color: "#f5c412", // Semi-transparent maroon
         strokeWidth: 1,
+        padding: "02rem",
+      });
+      annotation.show();
+    }
+
+    if (f) {
+      const annotation = annotate(f, {
+        animate: true,
+        type: "underline",
+        color: "#aa060d",
+        strokeWidth: 5,
       });
       annotation.show();
     }
@@ -28,7 +43,7 @@ const Home = () => {
         <section className="home_page-hero container">
           <div className="home_page-hero-title">
             <h1>
-              Enjoy your favortie dishes with <br />
+              Enjoy your favorite dishes with <br />
               <span id="moriah_chicken_grill">Moriah Chicken Grill</span>{" "}
             </h1>
             <p>
@@ -43,7 +58,6 @@ const Home = () => {
             </NavLink>
           </div>
         </section>
-
         <section className="home_page-hero-images-container container">
           <div className="home_page-hero-images">
             {/* Updated image order to match the provided design image */}
@@ -57,6 +71,79 @@ const Home = () => {
               loading="lazy"
             />
             <img src={Moriah_Fries} alt="Moriah_Fries" loading="lazy" />
+          </div>
+        </section>
+        <section className="home_page-stats-container container">
+          <div className="home_page-stats-contents">
+            <div className="home_page-stats-item">
+              <h2>30 +</h2>
+              <p>Meals</p>
+            </div>
+
+            <div className="home_page-stats-item">
+              <h2>21 +</h2>
+              <p>Drinks</p>
+            </div>
+
+            <div className="home_page-stats-item">
+              <h2>3</h2>
+              <p>Branches</p>
+            </div>
+
+            <div className="home_page-stats-item">
+              <h2>6256 +</h2>
+              <p>Clients Served</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="home_page-products container section">
+          <div className="home_page-products-title">
+            <span>Our Products</span>
+            <h2 className="home_page-section-heading"> {/* Apply flex properties to this h2 */}
+              <h2>From Our </h2>{" "}
+              <h2 id="home_section-heading">
+                experienced chefs
+              </h2>{" "}
+            </h2>
+          </div>
+          <div className="home_page-products-contents">
+            {homeProducts.map((product) => {
+              return (
+                <div className="home_page-product_card" key={product.id}>
+                  <div className="home_page-product-img">
+                    <img src={product.image} alt="Full grilled chicken" />
+                  </div>
+
+                  <div className="home_page-product-details">
+                    <div className="home_page-product-name">
+                      <h3>{product.productName}</h3>
+                    </div>
+
+                    <div className="home_page-product-rating">
+                      <span>
+                        <Star size={20} color="#f5c412" /> {product.av_rating}
+                      </span>
+                      <p>({product.total_rating} reviews)</p>
+                    </div>
+
+                    <div className="home_page-product-description">
+                      {product.description}
+                    </div>
+
+                    <div className="home_page-product-pricing">
+                      <span>KES {product.price}</span>
+                      <p>
+                        <ShoppingCart
+                          size={16}
+                          className="shopping_cart-icon"
+                        />
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </section>
       </div>
